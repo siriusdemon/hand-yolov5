f2a3c258b229dd703238f7e87929800dc72438d0
siriusdemon <mory2016@126.com>
2021-05-25 15:34:04 +0800

    add script

---
+++ script/crop_hand_from_video.py
@@ -0,0 +1,63 @@
+import os
+import os.path as osp
+import sys
+import handdet
+import decord
+import cv2
+
+
+""" Steps
+1. read the video
+2. every 2s get 1 frame
+3. create an directory with the name stem of the video
+4. crop hands from frames with a confidence threshold
+5. save hands into the directory
+"""
+
+if len(sys.argv) != 2:
+    print(
+        "Usage: python demo.py path/to/image\n"
+        "   or: python demo.py path/to/image-directory"
+    )
+    os._exit(0)
+
+# Step1
+video_path = sys.argv[1]
+video = decord.VideoReader(video_path)
+avg_fps = video.get_avg_fps()
+print(f"Read video from {video_path}")
+print(f"Average FPS: {avg_fps}")
+
+# Step2
+crop_per_second = 0.3           # I mean 1 frame per 2s
+interval = int(avg_fps / crop_per_second)
+frames = video[::interval]
+frames = frames.asnumpy()[:, :, :, ::-1]
+
+# Step3
+base = "result_crops"
+os.makedirs(base, exist_ok=True)
+crop_dir = osp.join(base, osp.basename(video_path).split('.')[0])
+os.makedirs(crop_dir, exist_ok=True)
+
+# Step3 + Step4 + Step5
+def extend_box(box):
+    # add more background to enable better classify
+    left, top, right, bottom = box
+    w = right - left
+    h = bottom - top
+    left = max(0, left - w // 6)
+    top = max(0, top - h // 6)
+    right = right + w // 6
+    bottom = bottom + h // 6
+    return left, top, right, bottom
+
+CONF = 0.3
+res = handdet.detect_many(frames)
+for i, (frame, det) in enumerate(zip(frames, res)):
+    det = [d for d in det if d['conf'] >= CONF]
+    for d in det: d['bbox'] = extend_box(d['bbox'])
+    for j, crop in enumerate(handdet.crop(frame, det)):
+        crop_path = osp.join(crop_dir, f"crop{j:02}_fr{i:03}.png")
+        cv2.imwrite(crop_path, crop)
+
